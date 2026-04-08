29d6dc7ab3585b9850639955df0b8e154fbc9134
Christoph Kahl <kahlei@hotmail.de>
2026-04-08 10:54:22 +0200

    removeod auth reference

--- lib/postal_codes_client.rb
+++ lib/postal_codes_client.rb
@@ -3,7 +3,6 @@
 require_relative "postal_codes_client/version"
 require_relative "postal_codes_client/configuration"
 require_relative "postal_codes_client/errors"
-require_relative "postal_codes_client/resources/auth"
 require_relative "postal_codes_client/resources/postal_codes"
 require_relative "postal_codes_client/client"
 
